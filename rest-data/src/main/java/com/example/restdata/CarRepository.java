1b5bab11a8c6b41bfc361ce3241b3f0eb6bc3e25
maniek <mpastuszka@gmail.com>
2021-08-24 14:39:28 +0200

    [change] - exposing rest repository under different URI

--- rest-data/src/main/java/com/example/restdata/CarRepository.java
+++ rest-data/src/main/java/com/example/restdata/CarRepository.java
@@ -3,6 +3,6 @@ package com.example.restdata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "auta", path = "samochody")
+@RepositoryRestResource(collectionResourceRel = "auta", path = "samochody")
 public interface CarRepository extends JpaRepository<Car, Long> {
 }
